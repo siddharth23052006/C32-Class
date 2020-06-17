186bc241d847f198ece76eca6e203e3f06a6765f
siddharth23052006 <siddharthshivakumar2@gmail.com>
2020-06-17 18:09:54 +0530

    added scoring and changed background according to time

--- sketch.js
+++ sketch.js
@@ -7,11 +7,14 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+//    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +48,27 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if (backgroundImg){
+        background(backgroundImg)
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("Score: " + score, width - 300, 50);
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +97,18 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+    var dateTime = responseJSON.datetime;
+    var hour = dateTime.slice(11,13);
+    console.log(hour);
+    if (hour >=6 && hour <= 19){
+        bg = "sprites/bg.png";
+    }
+    else
+        bg = "sprites/bg2.jpg";
+    backgroundImg = loadImage(bg);
 }
